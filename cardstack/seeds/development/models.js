356953c7bdbc3159c8c3e07defdb649b5215689b
Balint Erdi <balint.erdi@gmail.com>
2017-11-10 12:05:05 +0100

    Welcome message is shown

---
+++ cardstack/seeds/development/models.js
@@ -0,0 +1,71 @@
+const Factory = require('@cardstack/test-support/jsonapi-factory');
+
+module.exports = [
+  {
+    type: 'plugin-configs',
+    id: '@cardstack/ephemeral'
+  },
+  {
+    type: 'plugin-configs',
+    id: '@cardstack/models'
+  },
+  {
+    type: 'plugin-configs',
+    id: '@cardstack/jsonapi'
+  },
+  {
+    type: 'data-sources',
+    id: 'default',
+    attributes: {
+      'source-type': '@cardstack/ephemeral',
+      params: {
+        initialModels: initialModels()
+      }
+    }
+  },
+  {
+    type: 'plugin-configs',
+    id: '@cardstack/hub',
+    relationships: {
+      'default-data-source': {
+        data: { type: 'data-sources', id: 'default' }
+      }
+    }
+  }
+];
+
+function initialModels() {
+  let factory = new Factory();
+
+  factory.addResource('content-types', 'pages')
+    .withAttributes({
+      'routing-field': 'permalink'
+    })
+    .withRelated('fields', [
+      factory.addResource('fields', 'title')
+        .withAttributes({
+          'field-type': '@cardstack/core-types::string'
+        }),
+      factory.addResource('fields', 'body')
+        .withAttributes({
+          'field-type': '@cardstack/core-types::string'
+        }),
+      factory.addResource('fields', 'permalink')
+        .withAttributes({
+          'field-type': '@cardstack/core-types::string'
+        }),
+      factory.addResource('fields', 'main-query')
+        .withAttributes({
+          'field-type': '@cardstack/core-types::object'
+        }),
+    ]);
+
+  factory.addResource('pages').withAttributes({
+    permalink: " ",
+    title: "Welcome to Cardstack!",
+  });
+
+  return factory.getModels();
+
+}
+
